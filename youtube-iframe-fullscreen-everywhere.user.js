3d67fd9e7ee44ef9d7c149c85353d0e5db1a825c
pploni <80548307+pepeloni-away@users.noreply.github.com>
2023-05-17 22:34:32 +0300

    Create youtube-iframe-fullscreen-everywhere.user.js

---
+++ youtube-iframe-fullscreen-everywhere.user.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name        Youtube Iframe Fullscreen Everywhere
+// @namespace   https://github.com/pepeloni-away
+// @author      pploni
+// @run-at      document-start
+// @insert-into page
+// @version     1.0
+// @description Allow fullscreen for youtube iframes embedded on any page
+// @grant       none
+// @match       *://*/*
+// ==/UserScript==
+
+const urls = [ // based on https://github.com/mwisnicki/userscripts/blob/master/allowfullscreen-youtube-embed.user.js
+  "https://www.youtube.com/embed/",
+  "https://youtube.com/embed/",
+  "https://www.youtube-nocookie.com/embed/"
+].join("|"),
+      allowFullscreen = (iframe) => iframe.setAttribute("allowFullscreen", "")
+
+Object.defineProperty(HTMLIFrameElement.prototype, "src", {
+    set: new Proxy(Object.getOwnPropertyDescriptor(HTMLIFrameElement.prototype, "src").set, {
+        apply(target, thisArg, args) {
+            // console.log("set iframe src to:", ...arguments)
+            if (RegExp(urls).test(args[0])) allowFullscreen(thisArg)
+            return target.apply(thisArg, args)
+        }
+    }),
+    get: Object.getOwnPropertyDescriptor(HTMLIFrameElement.prototype, "src").get,
+    configurable: true
+})
+HTMLIFrameElement.prototype.setAttribute = new Proxy(HTMLIFrameElement.prototype.setAttribute, {
+    apply(target, thisArg, args) {
+        if (args[0] === "src") {
+            // console.log("set iframe src attribute to:", ...arguments)
+            if (RegExp(urls).test(args[1])) allowFullscreen(thisArg)
+        }
+        return target.apply(thisArg, args)
+    }
+})
