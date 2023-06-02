87570fbed119d7cc074547d680d8d89dc24fec80
pploni <80548307+pepeloni-away@users.noreply.github.com>
2023-06-02 19:03:09 +0300

    protect console and refactor

--- no-debugger-abuse.user.js
+++ no-debugger-abuse.user.js
@@ -4,37 +4,80 @@
 // @author      pploni
 // @run-at      document-start
 // @insert-into page
-// @version     1.3
+// @version     1.4
 // @description Disable and optinally log calls to javascript debugger
 // @grant       none
 // @match       *://*/*
 // ==/UserScript==
 
-const logs = false,
-      details = true,
-      here = (url => { url = url.replace('https://', ''); return url.length < 75 ? url : url.substring(0, 75) + "..." })(location.href),
-      {log, info, debug, warn} = console,
-      l = (...args) => (debug || log || info || warn)(...args)
+const logs = false
+const protectConsole = true // attempt to disable console.clear and to prevent changes to console.log
+const here = location.href.length < 88 ? location.href.substring(8) : location.href.substring(8, 88) + "..."
+let logw
+let clearw
 let lastCall
+try { // this will fail if ublock runs first and its filters set console.clear to undefined
+    logw = Function.prototype.bind.call(console.log, console)
+    clearw = new Proxy(Function.prototype.bind.call(console.clear, console), {
+        apply(target, thisArg, args) {
+            if (args[0] === "please") return Reflect.apply(...arguments)
+            /* Throw error to break inline scripts, since the function constructor proxy doesn't catch debugger statements in them.
+             * Try commenting the line if some page doesn't load, but i think absolutely nobody has any reason to use
+             * console.clear inside an inline script (or at all, really), unless it's used together with debugger
+             * and new Date().getTime() to try and block dev tools access. */
+            throw new TypeError("Baaaaka! I'm not clearing it.")
+        }
+    })
+    Object.defineProperties(self.console, {
+        "clear": {
+            configurable: false,
+            set() {
+                return
+            },
+            get() {
+                return clearw
+            }
+        },
+        "log": {
+            configurable: false,
+            set() {
+                return
+            },
+            get() {
+                return logw
+            }
+        }
+    })
+} catch {}
 self.Function.prototype.constructor = new Proxy(self.Function.prototype.constructor, {
    apply: function(target, thisArg, args) {
        /* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/caller#browser_compatibility
         * Function.prototype.caller is non-standard but supported by everything
-        * caller may access arguments object and its own caller (>ω^) (^_~) */
-       let fnContent, caller = this.apply?.caller, callerContent, date, diff
+        * When not in strict mode caller may access arguments object and its own caller.
+        * You can write thing.caller.caller.caller ... */
+       let fnContent, caller = this.apply?.caller, callerContent, now, diff
+       // eval(caller = function() {debugger})
        try {
            fnContent = args[0]
-           caller ||= thisArg
-           try { callerContent = caller.toString() } catch(e) { callerContent = e.toString() }
-           date = new Date()
-           lastCall ? diff = date.getTime() - lastCall.getTime() : diff = 0
-           logs ? details ? l(`debugger call on ${here}`, diff, `ms since last call\ncalled by`, caller, `content:\n${callerContent}`) :
-           l(`debugger call on ${here}`, diff, `ms since last call`) : null
-           lastCall = date
+           caller ||= thisArg // use thisArg if we dont't have access to caller; if thisArg is a function, it will include the caller somewhere
+           try { callerContent = String(caller) } catch(e) { callerContent = e.toString() }
+           now = new Date().getTime()
+           lastCall ? diff = now - lastCall : diff = 0
+           if (logs) {
+               logw(`debugger call on ${here}`, diff, `ms since last call\ncalled by`, caller, `caller content:\n${callerContent}`)
+           }
+           lastCall = now
        } catch (err) {
-           if (logs) l(err, '\ndebugger logger failed\narguments:', arguments)
+           if (logs) logw(err, `\ndebugger logger failed on ${here}\narguments:`, arguments)
        }
+       /* They call debugger by returning a function to the global context, followed by javascript bullshit, looks kinda like this:
+        * `(function() {return}["constructor"]("debugger"))`
+        * Doing it like this opens the debugger to the function the line above creates, not to their code that returns the line above,
+        * otherwise you would be able to debug their obfuscated code in the debugger.
+        * With that said, the single if below will catch most if not all of them,
+        * you can uncomment the next line that uses includes() if you need this to be more agressive */
        if ('debugger' === fnContent) return
+       // if (fnContent.includes('debugger')) return
        return target.apply(thisArg, args)
    }
 })
