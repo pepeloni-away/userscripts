fb2512ed54589986e31708f78b1b9b8a81cb29c4
pploni <80548307+pepeloni-away@users.noreply.github.com>
2023-06-06 23:37:50 +0300

    Create twitch-dvr-player.user.js

---
+++ twitch-dvr-player.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name        Twitch DVR player
+// @namespace   https://github.com/pepeloni-away
+// @icon        https://github.com/caeleel/twitch-dvr/raw/master/twitch-dvr%4048.png
+// @author      pploni
+// @run-at      document-idle
+// @insert-into page
+// @version     1.0
+// @description Unofficial userscript version of twitch-dvr chrome extension from https://github.com/caeleel/twitch-dvr
+// @require     https://raw.githubusercontent.com/caeleel/twitch-dvr/master/mux.js
+// @require     https://raw.githubusercontent.com/caeleel/twitch-dvr/master/index.js
+// @grant       none
+// @match       https://www.twitch.tv/*
+// ==/UserScript==
